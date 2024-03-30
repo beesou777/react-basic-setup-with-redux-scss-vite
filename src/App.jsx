59e5c77b395cdcbd9cb0df76a93e7328dc8500da
beesou777 <sbeesou@gmail.com>
2024-03-30 09:58:30 +0545

    feat: react basic setup for

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Home from "./pages/home/Home";
+
+function App() {
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
+
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
+
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
+
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
+
+            dispatch(getApiConfiguration(url));
+        });
+    };
+
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.scss";
+
+
+const Home = () => {
+    return (
+        <div className="homePage">
+           this is home page
+        </div>
+    );
+};
+
+export default Home;
